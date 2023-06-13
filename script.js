939ac0de7297990c06fdce60ac86d0bc62c9d7ae
Fab-gar <Fabionahuelgarcia@gmail.com>
2023-06-12 22:23:58 -0300

    chatgpt

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.querySelector("button.button-menu-toggle")
     .addEventListener("click", function() {
            document.querySelector(".nav-links").
                       classList.toggle("nav-links-responsive")})
+                      
+                      const imprimir = (texto) => 
+                      {console.log(texto);}
+                      imprimir("holamundo")
